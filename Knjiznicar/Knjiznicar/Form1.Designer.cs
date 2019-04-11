c7260dff98b485ee8a488fbe66078c4d4d0378df
Goran Trifunović <gtrrifunov@foi.hr>
2019-04-11 16:58:46 +0200

    Zadatak

---
+++ Knjiznicar/Knjiznicar/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Knjiznicar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKnjiga = new System.Windows.Forms.TextBox();
+            this.cmbIznajmljena = new System.Windows.Forms.ComboBox();
+            this.lstPopis = new System.Windows.Forms.ListBox();
+            this.btnDodaj = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtIznajmi = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cmbFilter = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Knjiga";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(166, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Iznajmljena?";
+            // 
+            // txtKnjiga
+            // 
+            this.txtKnjiga.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtKnjiga.Location = new System.Drawing.Point(229, 19);
+            this.txtKnjiga.Name = "txtKnjiga";
+            this.txtKnjiga.Size = new System.Drawing.Size(374, 38);
+            this.txtKnjiga.TabIndex = 2;
+            // 
+            // cmbIznajmljena
+            // 
+            this.cmbIznajmljena.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cmbIznajmljena.FormattingEnabled = true;
+            this.cmbIznajmljena.Location = new System.Drawing.Point(229, 81);
+            this.cmbIznajmljena.Name = "cmbIznajmljena";
+            this.cmbIznajmljena.Size = new System.Drawing.Size(164, 39);
+            this.cmbIznajmljena.TabIndex = 3;
+            // 
+            // lstPopis
+            // 
+            this.lstPopis.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lstPopis.FormattingEnabled = true;
+            this.lstPopis.ItemHeight = 31;
+            this.lstPopis.Location = new System.Drawing.Point(12, 162);
+            this.lstPopis.Name = "lstPopis";
+            this.lstPopis.Size = new System.Drawing.Size(381, 376);
+            this.lstPopis.TabIndex = 4;
+            // 
+            // btnDodaj
+            // 
+            this.btnDodaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnDodaj.Location = new System.Drawing.Point(421, 81);
+            this.btnDodaj.Name = "btnDodaj";
+            this.btnDodaj.Size = new System.Drawing.Size(258, 52);
+            this.btnDodaj.TabIndex = 5;
+            this.btnDodaj.Text = "Dodaj knjigu";
+            this.btnDodaj.UseVisualStyleBackColor = true;
+            this.btnDodaj.Click += new System.EventHandler(this.btnDodaj_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(740, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 31);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Iznajmi knjigu";
+            // 
+            // txtIznajmi
+            // 
+            this.txtIznajmi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtIznajmi.Location = new System.Drawing.Point(746, 72);
+            this.txtIznajmi.Name = "txtIznajmi";
+            this.txtIznajmi.Size = new System.Drawing.Size(374, 38);
+            this.txtIznajmi.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(862, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(258, 52);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Iznajmi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cmbFilter
+            // 
+            this.cmbFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.cmbFilter.FormattingEnabled = true;
+            this.cmbFilter.Location = new System.Drawing.Point(464, 226);
+            this.cmbFilter.Name = "cmbFilter";
+            this.cmbFilter.Size = new System.Drawing.Size(164, 39);
+            this.cmbFilter.TabIndex = 9;
+            this.cmbFilter.SelectedIndexChanged += new System.EventHandler(this.cmbFilter_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(432, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(217, 31);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Filter iznajmljeno";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(862, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(258, 52);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Vrati";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1166, 602);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbFilter);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtIznajmi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDodaj);
+            this.Controls.Add(this.lstPopis);
+            this.Controls.Add(this.cmbIznajmljena);
+            this.Controls.Add(this.txtKnjiga);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKnjiga;
+        private System.Windows.Forms.ComboBox cmbIznajmljena;
+        private System.Windows.Forms.ListBox lstPopis;
+        private System.Windows.Forms.Button btnDodaj;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtIznajmi;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox cmbFilter;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Knjiznicar/Knjiznicar/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Knjiznicar
+{
+    public partial class Form1 : Form
+    {
+        public string formatListe= "{0,-30}{1,-15}";
+        public class knjiga
+        {
+            public string nazivKnjige;
+            public string iznajmljeno;
+        }
+        public List<knjiga> popisKnjiga = new List<knjiga>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstPopis.Items.Add(String.Format(formatListe, "Knjiga", "Iznajmljena"));
+            cmbIznajmljena.Items.Add("Da");
+            cmbIznajmljena.Items.Add("Ne");
+
+            cmbFilter.Items.Add("Iznajmljena");
+            cmbFilter.Items.Add("Nije iznajmljena");
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            knjiga trenutniUnos = new knjiga();
+            trenutniUnos.nazivKnjige = txtKnjiga.Text;
+            trenutniUnos.iznajmljeno = cmbIznajmljena.Text;
+            popisKnjiga.Add(trenutniUnos);
+            lstPopis.Items.Add(String.Format(formatListe, trenutniUnos.nazivKnjige, trenutniUnos.iznajmljeno));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int brojac = 0; brojac < popisKnjiga.Count; brojac++)
+            {
+                if (txtIznajmi.Text == popisKnjiga[brojac].nazivKnjige)
+                {
+                   if (popisKnjiga[brojac].iznajmljeno != "Da")
+                    {
+                        popisKnjiga[brojac].iznajmljeno = "Da";
+                    }
+                }
+            }
+        }
+
+        private void cmbFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstPopis.Items.Clear();
+            if (cmbFilter.Text == "Iznajmljena") {
+                for (int brojac = 0; brojac < popisKnjiga.Count; brojac++)
+                {
+                    if (popisKnjiga[brojac].iznajmljeno == "Da")
+                    {
+                        lstPopis.Items.Add(string.Format(formatListe, popisKnjiga[brojac].nazivKnjige, popisKnjiga[brojac].iznajmljeno));
+                    }
+                }
+            }
+            else
+            {
+                for (int brojac = 0; brojac < popisKnjiga.Count; brojac++)
+                {
+                    if (popisKnjiga[brojac].iznajmljeno == "Ne")
+                    {
+                        lstPopis.Items.Add(string.Format(formatListe, popisKnjiga[brojac].nazivKnjige, popisKnjiga[brojac].iznajmljeno));
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int brojac = 0; brojac < popisKnjiga.Count; brojac++)
+            {
+                if (txtIznajmi.Text == popisKnjiga[brojac].nazivKnjige)
+                {
+                    if (popisKnjiga[brojac].iznajmljeno == "Da")
+                    {
+                        popisKnjiga[brojac].iznajmljeno = "Ne";
+                    }
+                }
+            }
+        }
+    }
+}
